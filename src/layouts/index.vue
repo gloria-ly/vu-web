286e44340a6ee6b65d52f1e83897401bda9887b5
luoyi <luoyiaj@qq.com>
2024-10-15 14:39:48 +0800

    feat: 框架搭建-1

--- src/layouts/index.vue
+++ src/layouts/index.vue
@@ -1,3 +1,8 @@
 <template>
-    <div>layout加载。。。。。</div>
+  <div>layout加载。。。。。</div>
+  <router-view v-slot="{ Component, route }">
+    <component :is="Component" :key="route" />
+  </router-view>
 </template>
+
+<script setup lang="ts"></script>
--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,20 @@
-import { createApp } from 'vue'
-import './style.css'
-import '@/style/index.less';
-import App from './App.vue'
+import { createApp } from 'vue';
+import './style.css';
+import '@/style/index.css';
+import App from './App.vue';
 import { setupStore } from '@/store';
 import router from '@/router';
+import i18n from '@/i18n';
 import { registerGlobComp } from '@/components/registerGlobComp';
+import 'dayjs';
 
 function bootstrap() {
-    const app = createApp(App);
-    setupStore(app);
-    registerGlobComp(app);
-    app.use(router);
-    app.mount('#app');
-  }
-  
-  bootstrap();
-  
+  const app = createApp(App);
+  setupStore(app);
+  registerGlobComp(app);
+  app.use(router);
+  app.use(i18n);
+  app.mount('#app');
+}
+
+bootstrap();
