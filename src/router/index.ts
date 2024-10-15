ad37b5643a8a33e07976beb1f687af7e514de0b7
luoyi <luoyiaj@qq.com>
2024-10-15 15:32:23 +0800

    feat: 框架搭建-2

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,40 +2,41 @@ import type { App } from 'vue';
 import { createRouter, createWebHistory, createWebHashHistory, RouteRecordRaw } from 'vue-router';
 import { asyncLayoutImport, asyncViewImport } from '@/router/helper/asyncComponentImport';
 
-const routes: Array<RouteRecordRaw> = [{
-  path: '/',
-  name: 'Home',
-  meta: {
-    title: 'Home',
-    public: false
-  },
-  component: asyncLayoutImport('index.vue'),
-  children: [
-    {
-      path: '/internetExposureSurface',
-      name: 'internetExposureSurface',
-      meta: {
-        title: '主页'
-      },
-      component: asyncViewImport('home.vue')
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    name: 'Home',
+    meta: {
+      title: 'Home',
+      public: false
     },
-  ]
-}]
+    component: asyncLayoutImport('index.vue'),
+    children: [
+      {
+        path: '/home',
+        name: 'home',
+        meta: {
+          title: '主页'
+        },
+        component: asyncViewImport('home.vue')
+      }
+    ]
+  }
+];
 
 const router = createRouter({
   history: createWebHashHistory(),
-  routes,
+  routes
 });
 
-router.beforeEach((to,from)=>{
+router.beforeEach((to, from) => {
   //条件
   // if(pass){
   // console.log('pass')
   //   // 不再使用next()放行
   // }else{
-  //     return false 
+  //     return false
   // }
-})
-
+});
 
 export default router;
--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,47 +1,47 @@
-import Axios, { AxiosRequestConfig, AxiosResponse, AxiosInstance } from "axios";
+import Axios, { AxiosRequestConfig, AxiosResponse, AxiosInstance } from 'axios';
 import { message } from 'ant-design-vue/es';
-import { getEnv } from "./env";
+import { getEnv } from './env';
 
 // create an axios instance
-    const axios: AxiosInstance = Axios.create({
-        baseURL:getEnv().VITE_API_BASE_URL,
-        timeout: 20000, // 请求超时 20s
-    });
-  
-  // 前置拦截器（发起请求之前的拦截）
-  axios.interceptors.request.use(
-    (response) => {
-      /**
-       * 根据你的项目实际情况来对 config 做处理
-       * 这里对 config 不做任何处理，直接返回
-       */
-      return response;
-    },
-    (error) => {
-      return Promise.reject(error);
+const axios: AxiosInstance = Axios.create({
+  baseURL: getEnv().VITE_API_BASE_URL,
+  timeout: 20000 // 请求超时 20s
+});
+
+// 前置拦截器（发起请求之前的拦截）
+axios.interceptors.request.use(
+  (response) => {
+    /**
+     * 根据你的项目实际情况来对 config 做处理
+     * 这里对 config 不做任何处理，直接返回
+     */
+    return response;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// 后置拦截器（获取到响应时的拦截）
+axios.interceptors.response.use(
+  (response) => {
+    /**
+     * 根据你的项目实际情况来对 response 和 error 做处理
+     * 这里对 response 和 error 不做任何处理，直接返回
+     */
+    return response;
+  },
+  (error) => {
+    if (error.response && error.response.data) {
+      const code = error.response.status;
+      const msg = error.response.data.message;
+      message.error(`Code: ${code}, Message: ${msg}`);
+      console.error(`[Axios Error]`, error.response);
+    } else {
+      message.error(`${error}`);
     }
-  );
-  
-  // 后置拦截器（获取到响应时的拦截）
-  axios.interceptors.response.use(
-    (response) => {
-      /**
-       * 根据你的项目实际情况来对 response 和 error 做处理
-       * 这里对 response 和 error 不做任何处理，直接返回
-       */
-      return response;
-    },
-    (error) => {
-      if (error.response && error.response.data) {
-        const code = error.response.status;
-        const msg = error.response.data.message;
-        message.error(`Code: ${code}, Message: ${msg}`);
-        console.error(`[Axios Error]`, error.response);
-      } else {
-        message.error(`${error}`);
-      }
-      return Promise.reject(error);
-    }
-  );
-  
-  export default axios;
+    return Promise.reject(error);
+  }
+);
+
+export default axios;
